07b96a6d521cfba93c4b528c6909e64cc03ab8cf
Kamaludin Khoir <khoirkamaludin@gmail.com>
2021-11-13 06:51:50 +0700

    reconfigure registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use App\Models\School;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -65,12 +66,21 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $left_id = 'SCH'.date('y').date('m');
+        $school = new School();
+        $school_id = $school->createNewSchoolID($left_id);
+
+        $school = School::create([
+            'school_id' => $school_id,
+            'school_name' => $data['school_name']
+        ]);
+
         return User::create([
             'email' => $data['email'],
             'username' => $data['username'],
             'password' => Hash::make($data['password']),
-            'role' => json_encode(['ADMIN']),
-            'school_id' => 1,
+            'role' => 'ADMIN',
+            'school_id' => $school->id,
         ]);
     }
 }
---
+++ app/Models/School.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class School extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function createNewSchoolID($left_id)
+    {
+        $latest_right_id = DB::select("select MAX(RIGHT(school_id,3)) as max_id from schools where LEFT(school_id, 7) = '".$left_id."'");
+        $new_right_id = "";
+        if(count($latest_right_id)>0) {
+            $int_new_right_id = ((int)$latest_right_id[0]->max_id)+1;
+            $new_right_id = sprintf("%03s", $int_new_right_id);
+        } else {
+            $new_right_id = "001";
+        }   
+        return $left_id."".$new_right_id;
+    }
+}
